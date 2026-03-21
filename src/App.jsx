a17803fc216ac436ffc6400e03ad7f7a195a2202
krish <krishtaliyan132@gmail.com>
2026-03-22 02:00:16 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-green-400">
+        NearBits 🚀 Ready
+      </h1>
+    </div>
+  );
+}
+
+export default App;
